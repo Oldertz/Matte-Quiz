bf13c7bf79ddb2afd1e7500959fcbe10735d3871
Oldertz <viggo.oldertz@gmail.com>
2017-09-22 12:48:15 +0200

    Start - ta emot användar svar

--- Matte Quiz/Program.cs
+++ Matte Quiz/Program.cs
@@ -8,8 +8,25 @@ namespace Matte_Quiz
 {
     class Program
     {
+        static int RequestInput() //tar emot användar svar
+        {
+            int playerAnswer;
+            while (!int.TryParse(Console.ReadLine(), out playerAnswer))
+            {
+                Console.WriteLine("*ERROR* Kan ej läsa, använd siffror.");
+            }
+
+            return playerAnswer;
+        }
+
         static void Main(string[] args)
         {
+
+            Console.Write("Hur många matematiska problem vill du lösa?");
+           
         }
     }
 }
+
+
+
