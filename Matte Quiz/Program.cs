6e30b8c1ddf78ad6b3c2008634b28e35d21fd83d
Oldertz <viggo.oldertz@gmail.com>
2017-09-28 15:04:32 +0200

    Skapat while loop med frågedelen av spelet. Svarar du rätt ökar poängen

--- Matte Quiz/Program.cs
+++ Matte Quiz/Program.cs
@@ -8,7 +8,7 @@ namespace Matte_Quiz
 {
     class Program
     {
-        static int RequestInput() //tar emot svar och ger felmeddelande om man skriver in en bokstav
+        static int RequestInput()                             //tar emot svar och ger felmeddelande om man skriver in en bokstav
         {
             int playerAnswer;
             while (!int.TryParse(Console.ReadLine(), out playerAnswer))
@@ -23,17 +23,39 @@ namespace Matte_Quiz
         {
             Random randomGenerator = new Random();
 
-            int nummer1 = randomGenerator.Next(11);
-            int nummer2 = randomGenerator.Next(11);
-            int svar;
-            int antalFrågor;
-            int antalFrågorKvar;
-            int antalRätt = 0;
+            int number1 = randomGenerator.Next(11);
+            int number2 = randomGenerator.Next(11);
+            int answer;
+            int playerAnswer; 
+            int numQuestions;
+            int numQuestionsLeft;
+            int correctAnswer = 0;
 
             Console.Write("Hur många matematiska problem vill du lösa? ");
-            antalFrågorKvar = RequestInput();
+            numQuestions = Convert.ToInt32(RequestInput());
+            numQuestionsLeft = numQuestions;
 
-            Console.WriteLine(antalFrågorKvar);
+
+            while (numQuestionsLeft > 0)                     //Loopen som hanterar frågedelen av spelet. Svarar du rätt ökar det poängen.
+            {
+
+                Console.Write("Vad är " + number1 + " * " + number2 + "? ");
+                answer = number1 * number2;
+                playerAnswer = RequestInput();
+                if (answer == playerAnswer)
+                {
+                    Console.WriteLine(playerAnswer + " är rätt, bra jobbat!");
+                    correctAnswer++;
+                }
+                else
+                {
+                    Console.WriteLine(playerAnswer + " Är fel, försök igen..");
+                }
+                Console.ReadKey();
+                numQuestionsLeft--;
+                number1 = randomGenerator.Next(11);
+                number2 = randomGenerator.Next(11);
+            }
 
         }
     }
