8df3db12501ed873d9d27f5ce6185ee0fdea19ca
Oldertz <viggo.oldertz@gmail.com>
2017-09-28 14:23:00 +0200

    felmeddelande vid bokstavsinput

--- Matte Quiz/Program.cs
+++ Matte Quiz/Program.cs
@@ -8,7 +8,7 @@ namespace Matte_Quiz
 {
     class Program
     {
-        static int RequestInput() //tar emot användar svar
+        static int RequestInput() //tar emot svar och ger felmeddelande om man skriver in en bokstav
         {
             int playerAnswer;
             while (!int.TryParse(Console.ReadLine(), out playerAnswer))
@@ -21,9 +21,13 @@ namespace Matte_Quiz
 
         static void Main(string[] args)
         {
+            Random randomgen = new Random();
 
-            Console.Write("Hur många matematiska problem vill du lösa?");
-           
+      
+
+            Console.Write("Hur många matematiska problem vill du lösa? ");
+            RequestInput();
+
         }
     }
 }
